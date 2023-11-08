472eb4e0ca95480307d80153fc42069da38f0885
Jack Phillips <jack.p.phillips@gmail.com>
2023-11-07 23:42:02 -0700

    Removed repeating includes

--- Velvet/src/Velvet/ImGui/ImGuiLayer.cpp
+++ Velvet/src/Velvet/ImGui/ImGuiLayer.cpp
@@ -1,8 +1,6 @@
 #include <vlpch.h>
 #include "Velvet/ImGui/ImGuiLayer.h"
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include <imgui.h>
 #include <imgui_impl_opengl3.h>
 #include <imgui_impl_glfw.h>
