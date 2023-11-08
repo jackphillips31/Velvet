797141c3ba6bcafb379fb21aaea2295cbfc141d9
Jack Phillips <jack.p.phillips@gmail.com>
2023-11-08 09:41:55 -0700

    Added Input and Keycodes to main Velvet header

--- Velvet/src/Velvet.h
+++ Velvet/src/Velvet.h
@@ -4,6 +4,11 @@
 #include "Velvet/Core/Application.h"
 #include "Velvet/Core/Layer.h"
 #include "Velvet/Core/Log.h"
+
+#include "Velvet/Core/Input.h"
+#include "Velvet/Core/KeyCodes.h"
+#include "Velvet/Core/MouseButtonCodes.h"
+
 #include "Velvet/ImGui/ImGuiLayer.h"
 
 //----------ENTRY POINT----------
