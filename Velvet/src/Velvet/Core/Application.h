18e2c2f746ea494a2cb2d2f59b35684b6cb0c978
Jack Phillips <jack.p.phillips@gmail.com>
2023-11-22 16:36:26 -0700

    Render API implementation

--- Velvet/src/Velvet/Core/Application.h
+++ Velvet/src/Velvet/Core/Application.h
@@ -10,6 +10,7 @@
 #include "Velvet/ImGui/ImGuiLayer.h"
 
 #include "Velvet/Renderer/Shader.h"
+#include "Velvet/Renderer/Buffer.h"
 
 namespace Velvet {
 
@@ -37,8 +38,10 @@ namespace Velvet {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
