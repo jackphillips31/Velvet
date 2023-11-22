0d662db935f4f46700ee74c9628ee54d169a1bc8
Jack Phillips <jack.p.phillips@gmail.com>
2023-11-22 15:45:21 -0700

    Rainbow Triangle

--- Velvet/src/Velvet/Core/Application.cpp
+++ Velvet/src/Velvet/Core/Application.cpp
@@ -49,8 +49,11 @@ namespace Velvet {
 
 			layout(location = 0) in vec3 a_Position;
 
+			out vec3 v_Position;
+
 			void main()
 			{
+				v_Position = a_Position;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
@@ -59,9 +62,11 @@ namespace Velvet {
 
 			layout(location = 0) out vec4 color;
 
+			in vec3 v_Position;
+
 			void main()
 			{
-				color = vec4(0.8, 0.2, 0.3, 1.0);
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
 			}
 		)";
 
