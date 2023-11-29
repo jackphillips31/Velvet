45c482bc7ecb394975d44a88a339eeaf68aa1f6c
Jack Phillips <jack.p.phillips@gmail.com>
2023-11-29 14:38:11 -0700

    Updated Velvet Header File

--- Velvet/src/Velvet.h
+++ Velvet/src/Velvet.h
@@ -2,17 +2,31 @@
 // For use by Velvet applications
 
 #include "Velvet/Core/Application.h"
+#include "Velvet/Core/Layer.h"
 #include "Velvet/Core/Log.h"
 
 #include "Velvet/Core/Input.h"
 #include "Velvet/Core/KeyCodes.h"
 #include "Velvet/Core/MouseButtonCodes.h"
 
+#include "Velvet/Events/Event.h"
 #include "Velvet/Events/KeyEvent.h"
 #include "Velvet/Events/MouseEvent.h"
 
-#include "Velvet/Renderer/Renderer.h"
+#include "Velvet/ImGui/ImGuiLayer.h"
 
-//----------ENTRY POINT----------
-//-------------------------------
+//----------------- RENDERER ------------------
+//---------------------------------------------
+#include "Velvet/Renderer/Renderer.h"
+#include "Velvet/Renderer/RenderCommand.h"
+
+#include "Velvet/Renderer/Buffer.h"
+#include "Velvet/Renderer/Shader.h"
+#include "Velvet/Renderer/VertexArray.h"
+
+#include "Velvet/Renderer/OrthographicCamera.h"
+
+
+//---------------- ENTRY POINT ----------------
+//---------------------------------------------
 #include "Velvet/Core/EntryPoint.h"
