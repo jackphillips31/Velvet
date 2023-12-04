4a181b2944b5f6a67b82c2c6648b18e099b3f1dc
Jack Phillips <jack.p.phillips@gmail.com>
2023-12-04 15:18:51 -0700

    Added shader file compatibility

--- Velvet/src/Velvet/Renderer/Shader.cpp
+++ Velvet/src/Velvet/Renderer/Shader.cpp
@@ -6,6 +6,18 @@
 
 namespace Velvet {
 
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:	VL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:	return new OpenGLShader(filepath);
+		}
+
+		VL_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
